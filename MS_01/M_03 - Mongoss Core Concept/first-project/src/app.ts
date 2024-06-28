83a01e9c28e8021b25ccb4170f0e1f5af758ad9b
abushamacd <abushamacd@gmail.com>
2024-06-28 07:39:26 +0600

    m3 v3 - Eslint setup

--- MS_01/M_03 - Mongoss Core Concept/first-project/src/app.ts
+++ MS_01/M_03 - Mongoss Core Concept/first-project/src/app.ts
@@ -1,6 +1,6 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-// import { StudentRoutes } from './app/modules/student/student.route';
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 const app: Application = express();
 
@@ -9,13 +9,13 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-// app.use('/api/v1/students', StudentRoutes);
+app.use('/api/v1/students', StudentRoutes);
 
 const getAController = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get("/", getAController);
+app.get('/', getAController);
 
 export default app;
